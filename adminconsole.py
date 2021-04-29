05e4ce2a3155362de0adb9aa7218fdac5bb140b7
Vinod Kumar Kotha <vk21125@corp.ojas-it.com>
2021-04-29 10:35:48 +0530

    first_commit

---
+++ adminconsole.py
@@ -0,0 +1,11 @@
+class adminconsole:
+    def __init__(self):
+        pass
+    def create_emp_account(self):
+        pass
+    def list_emp_details(self):
+        pass
+    def emp_leave_request(self):
+        pass
+    def emp_account_del(self):
+        pass
