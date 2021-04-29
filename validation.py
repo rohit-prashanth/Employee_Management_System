2cc0febeb43bc402bcefe4b9f9a9cf2da4cb5cf4
Salman Shaik <ss21100@corp.ojas-it.com>
2021-04-29 11:02:29 +0530

    first validation

---
+++ validation.py
@@ -0,0 +1,9 @@
+import datetime
+class validation:
+    def __init__(self):
+        pass
+    def emp_creation validation(username,email):
+        pass
+
+    def emp_leave_request_validation(emp_username, from_date, to_date):
+        pass
