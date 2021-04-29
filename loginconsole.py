3a13cb23d9d775bfa245db43e77416415fbace50
Tejeshwari <tejeshwariteja@gmail.com>
2021-04-29 11:28:26 +0530

    first login console commit

---
+++ loginconsole.py
@@ -0,0 +1,12 @@
+class LoginConsole:
+    def request_leave():
+        pass
+
+    def emp_search(emp_username):
+        pass
+    
+
+    def change_password(emp_username):
+        pass
+    
+                
