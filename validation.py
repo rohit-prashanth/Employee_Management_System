9d3c2a54d1dfdd6ba5aa8d317414129090c34fa2
Salman Shaik <ss21100@corp.ojas-it.com>
2021-05-10 20:20:51 +0530

    validation over

--- validation.py
+++ validation.py
@@ -3,6 +3,7 @@ import re
 from data_base import Database
 from getpass import getpass
 from string import punctuation
+# from loginconsole import LoginConsole
 #Database().FUNCTION NAME
 
 class Validation:
@@ -50,7 +51,7 @@ class Validation:
     def emp_passwordvalidation(password):
 
         while True:
-            password = getpass("enter u r password::-")
+            password = input("enter u r password::-")
 
             if ' ' in password:
                 print( 'there is a space in password')
@@ -76,6 +77,144 @@ class Validation:
                 break
         print(password)
 
+    def emp_salaryvalidation(self):
+        while True:
+            try:
+                salary = float(input("enter salary"))
+                if salary:
+                    return salary
+            except:
+                print("enter valid salary ")
+                continue
+
+    def emp_pfno_validation(self):
+        while True:
+            pfno = input("enter pf no")
+            if pfno.isalpha():
+                continue
+            if pfno.isnumeric():
+                continue
+            if pfno.isalnum():
+                return pfno
+            else:
+                continue
+
+    def date_validation(self):
+        while True:
+            date_string = input("enter date in YYYY-MM-DD format")
+            format = "%Y-%m-%d"
+            try:
+                if datetime.datetime.strptime(date_string, format):
+                    return datetime.datetime.strptime(date_string, format)
+                    break
+            except:
+                print("Incorrect data format, should be YYYY-MM-DD")
+                continue
+
+    def panid_validation(self):
+        while True:
+            pfno = input("enter pan no")
+            if pfno.isalpha():
+                continue
+            if pfno.isnumeric():
+                continue
+            if pfno.isalnum():
+                return pfno
+            else:
+                continue
+
+# obj = Validation()
+# obj.emp_emailvalidation()
+# obj.emp_passwordvalidation()
+
+class Loginusername_validation:
+
+    def username_validation(self):
+        while True:
+            username = input("enter username")
+            password = input("enter password")
+            us_data = Database().read_table("Emp_Creation_Table")
+            dic = {}
+            for i in us_data:
+                dic[i[2]] = i[4]
+                if username in dic.keys():
+                    if password == dic[username]:
+                        print("success full")
+                    else:
+                        print("entered credentials are wrong")
+                        continue
+
+
+    def todate_validation(self):
+        while True:
+            date_string = input("enter to date in YYYY-MM-DD format")
+            format = "%Y-%m-%d"
+            try:
+                if datetime.datetime.strptime(date_string, format):
+                    if from_date[0:10]<=str(datetime.datetime.strptime(date_string, format))[0:10]:
+                        return datetime.datetime.strptime(date_string, format)
+                        break
+                    else:
+                        print("enter valid date,it should be more than the current date")
+                        continue
+            except:
+                print("Incorrect data format, should be YYYY-MM-DD")
+                continue
+
+
+    def leave_balance(self):
+        le=LoginConsole.request_leave()
+        lste=[]
+        for i in le:
+            lste.append(i[2,3 ])
+
+class newpassword:
+
+    def emp_passwordvalidation(password):
+
+        while True:
+            password = input("enter u r password::-")
+
+            if ' ' in password:
+                print('there is a space in password')
+                continue
+
+            if len(password) not in range(8, 17):
+                print('password should between in min 8 & max 16 characters')
+                continue
+
+            special_chars = [True for x in password if x in punctuation]
+            if len(special_chars) == 0:
+                print('your password should have one special character')
+                continue
+
+            nums = any(x.isdigit() for x in password)
+            if not nums:
+                print("you should have atleast 1 digit in your password")
+                continue
+
+            else:
+
+                return password
+                break
+        print(password)
+
+
+    def user_input(self):
+        while True:
+            userinpt=input("enter u r input")
+            if userinpt=="yes" or userinpt=='YES' or userinpt=="no" or userinpt== "NO":
+                print("valid i/p")
+                break
+
+
+            else:
+                print("invalid user  please enter again")
+                continue
+
+
+obj4=newpassword()
+obj4.user_input()
 
 
 
@@ -83,31 +222,7 @@ class Validation:
 
 
 
-obj = Validation()
-print(obj.emp_passwordvalidation())
 
 
 
 
-
-
-# password length be min 8 char and max length is 16 in that upper&lower cases &special symbols and numbers
-#def changepassword():
-
-
-
-
-
-
-# class validation:
-#     def __init__(self):
-#         pass
-#     def emp_usernamevalidation(username):
-#
-#
-#     def emp_salaryvalidation(self):
-#
-#     def emp_datevalidation(self):
-#
-#     def emp_leave_request_validation(emp_username, from_date, to_date):
-#         print("it is a valid ")
